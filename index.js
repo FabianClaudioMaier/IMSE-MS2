742983791d5cfcf6fcf434af36ee440e0021e46e
Fabian Maier <a11907564@univie.ac.at>
2026-01-10 15:31:19 +0100

    compose added

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+const http = require("http");
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello from IMSE-MS2 Docker container 👋\n");
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
